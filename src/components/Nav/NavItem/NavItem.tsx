f03fc3678cbc2d9c8857463033ddaaf8373f1ca7
Saviovd <savio.pinheiroal@gmail.com>
2024-05-09 16:38:38 -0400

    update: new habilities responsiviness

--- src/components/Nav/NavItem/NavItem.tsx
+++ src/components/Nav/NavItem/NavItem.tsx
@@ -41,7 +41,7 @@ const NavItem = ({ name, isSelected, setIsSelected, pagePosition }: INavItemProp
                smooth={true}
                spy={true}
                duration={500}
-               offset={typeof window !== 'undefined' && window.innerWidth < 768 || name !== t('Header.habilities') ? -100 : -180}>
+               offset={typeof window !== 'undefined' && window.innerWidth < 768 || name !== t('Header.habilities') ? -100 : -150}>
                {name}
             </ScrollLink>
       </NavItemStyle>
