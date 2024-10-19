72b072f56a71e02c277d4c13bd152f7b6d70cd3e
Saviovd <savio.pinheiroal@gmail.com>
2024-10-18 20:11:45 -0400

    [add] - certifications with paginations

---
+++ src/components/Certifications/index.tsx
@@ -0,0 +1,147 @@
+import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import Title from '../Title';
+import PaginationControls from '../PaginationControls';
+import GraduationCard from '../GraduationCard';
+import CourseCard from '../CourseCard';
+import { CertificationsStyle, Menu } from './styles';
+import { SlGraduation } from 'react-icons/sl';
+import { Course, Graduation } from '@/types/personaldata';
+
+type ListType = 'graduation' | 'courses';
+
+interface ICertificationsProps {
+   graduations: Graduation[];
+   courses: Course[];
+}
+
+const Certifications = ({ graduations, courses }: ICertificationsProps) => {
+   const [activatedList, setActivatedList] = useState<ListType>('graduation');
+   const [currentPage, setCurrentPage] = useState(1);
+   const itemsPerPage = 6;
+
+   const totalPages = Math.ceil(
+      (activatedList === 'graduation' ? graduations.length : courses.length) /
+         itemsPerPage
+   );
+
+   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   const getPaginatedItems = (items: any[], currentPage: number) => {
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      return items.slice(startIndex, startIndex + itemsPerPage);
+   };
+
+   const handleListChange = (listType: ListType) => {
+      setActivatedList(listType);
+      setCurrentPage(1);
+   };
+
+   const handleNextPage = () =>
+      setCurrentPage((prev) => Math.min(prev + 1, totalPages));
+   const handlePreviousPage = () =>
+      setCurrentPage((prev) => Math.max(prev - 1, 1));
+
+   const categories = [
+      { label: 'Graduação', value: 'graduation' },
+      { label: 'Cursos', value: 'courses' },
+   ];
+
+   return (
+      <CertificationsStyle>
+         <Title
+            text={'Certificações'}
+            icon={<SlGraduation className='icon' />}
+            className='title'
+         />
+
+         <Menu>
+            {categories.map((category) => (
+               <li
+                  key={category.value}
+                  className={`menu-item ${
+                     activatedList === category.value ? 'active' : ''
+                  }`}
+                  onClick={() => handleListChange(category.value as ListType)}
+               >
+                  {category.label}
+               </li>
+            ))}
+         </Menu>
+
+         <div className='list-box'>
+            <AnimatePresence mode='wait'>
+               {activatedList === 'graduation' && (
+                  <motion.ul
+                     className='list'
+                     key={`graduation-${currentPage}`}
+                     initial={{ opacity: 0 }}
+                     animate={{ opacity: 1 }}
+                     exit={{ opacity: 0 }}
+                     transition={{ duration: 0.5 }}
+                  >
+                     {getPaginatedItems(graduations, currentPage).map(
+                        (graduation, i) => (
+                           <motion.li
+                              className='graduation'
+                              key={i}
+                              custom={i}
+                              initial='hidden'
+                              animate='visible'
+                              exit='exit'
+                              variants={{
+                                 hidden: { opacity: 0, y: 20 },
+                                 visible: { opacity: 1, y: 0 },
+                                 exit: { opacity: 0, y: 20 },
+                              }}
+                           >
+                              <GraduationCard graduation={graduation} />
+                           </motion.li>
+                        )
+                     )}
+                  </motion.ul>
+               )}
+
+               {activatedList === 'courses' && (
+                  <motion.ul
+                     className='list'
+                     key={`courses-${currentPage}`}
+                     initial={{ opacity: 0 }}
+                     animate={{ opacity: 1 }}
+                     exit={{ opacity: 0 }}
+                     transition={{ duration: 0.5 }}
+                  >
+                     {getPaginatedItems(courses, currentPage).map(
+                        (course, i) => (
+                           <motion.li
+                              className='course'
+                              key={i}
+                              custom={i}
+                              initial='hidden'
+                              animate='visible'
+                              exit='exit'
+                              variants={{
+                                 hidden: { opacity: 0, y: 20 },
+                                 visible: { opacity: 1, y: 0 },
+                                 exit: { opacity: 0, y: 20 },
+                              }}
+                           >
+                              <CourseCard course={course} />
+                           </motion.li>
+                        )
+                     )}
+                  </motion.ul>
+               )}
+            </AnimatePresence>
+
+            <PaginationControls
+               currentPage={currentPage}
+               totalPages={totalPages}
+               onNextPage={handleNextPage}
+               onPreviousPage={handlePreviousPage}
+            />
+         </div>
+      </CertificationsStyle>
+   );
+};
+
+export default Certifications;
