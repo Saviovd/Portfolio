cc5002f004022d5438d585134aee980abe26ae63
Saviovd <savio.pinheiroal@gmail.com>
2024-12-10 17:03:44 -0400

    👌 IMPROVE: update not found page colors

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -74,13 +74,13 @@ const ErrorPageStyle = styled.main`
          &.shallow {
             left: 1px;
             z-index: 2;
-            text-shadow: 1px 0 0 red;
+            text-shadow: 1px 0 0 rgb(var(--light-green));
             animation-duration: 0.95s;
          }
          &.deep {
             left: 0;
             z-index: 3;
-            text-shadow: 1px 0 0 cyan;
+            text-shadow: 1px 0 0 rgb(var(--light-blue));
             mix-blend-mode: darken;
             animation-duration: 1.1s;
             animation-delay: 0.2s;
@@ -93,7 +93,7 @@ const ErrorPageStyle = styled.main`
          transform: translate(-50%, -50%);
          width: 100vw;
          height: 3px;
-         background-color: black;
+         background-color: rgba(var(--light-green));
          animation: line-move 10s ease-out infinite;
       }
 
@@ -111,12 +111,12 @@ const ErrorPageStyle = styled.main`
 
       .glitch.darken {
          .symbol {
-            color: black;
-            background-color: rgb(var(--secondary-white));
+            color: rgba(var(--light));
+            background-color: rgb(var(--light-green));
          }
 
          .line {
-            background-color: white;
+            background-color: rgb(var(--dark));
          }
       }
 
