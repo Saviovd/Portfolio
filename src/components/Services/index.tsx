dba33349db0d896a3191305c21dc8fdf63e6d5a6
Saviovd <savio.pinheiroal@gmail.com>
2024-12-12 16:32:12 -0400

    📦 NEW:  translations in services section

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,24 +1,23 @@
 import { ServicesList, ServicesPresentation, ServicesStyle } from './styles';
 import data from '@/data/services.json';
 import ServiceCard from '../ServiceCard';
+import { useTranslation } from 'react-i18next';
 const { services } = data;
 
 const Services = () => {
+   const { t } = useTranslation();
+
    return (
       <>
          <ServicesStyle id='services'>
             <div className='services-container'>
                <ServicesPresentation>
                   <h3 className='title'>
-                     Transformo Código em <br />
-                     <span className='xp'>Experiências Incríveis</span>
+                     {t('Services.transform')} <br />
+                     <span className='xp'> {t('Services.experiences')}</span>
                   </h3>
                   <p className='text'>
-                     Com foco em design intuitivo e performance, crio soluções
-                     que não só funcionam, mas surpreendem, elevando a interação
-                     e o engajamento com seu produto. Explore abaixo como meus
-                     serviços podem transformar sua ideia em uma experiência
-                     única para o seu cliente.
+                     {t('Services.description')}
                      <br />
                   </p>
                </ServicesPresentation>
